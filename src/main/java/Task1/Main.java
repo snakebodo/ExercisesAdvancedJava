daeeee71821beb72151301f785a60663813f4ca4
bogda <bogdanhalici1989@gmail.com>
2022-11-20 21:22:59 +0000

    1st commit

--- src/main/java/Task1/Main.java
+++ src/main/java/Task1/Main.java
@@ -1,10 +1,10 @@
-/*package Task1;
+package Task1;
 
 import Task1.Point2D;
 import Task1.Point3D;
 
 public class Main {
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
 
         Point2D point2D = new Point2D( 5.25f, 8.40f);
         Point3D point3D = new Point3D(point2D.getX(), point2D.getY(), 3.47f);
@@ -12,3 +12,9 @@ public class Main {
         System.out.println(point3D);
     }
 }*/
+    public static void main(String[] args) {
+        Line example = new Line(2.34f, 4.56f, 6.45f, 7.44f);
+        example.getLength();
+        System.out.println(example.getLength() + "\n" + example);
+    }
+}
